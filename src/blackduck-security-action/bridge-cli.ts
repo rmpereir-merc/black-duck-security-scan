0cac231e9b0c29a405c840cd0ee73674f3e3d5db
Gourav Jangra <gouravj@blackduck.com>
2024-10-28 16:02:34 +0530

    env and folder changes

--- src/blackduck-security-action/bridge-cli.ts
+++ src/blackduck-security-action/bridge-cli.ts
@@ -90,7 +90,6 @@ export class Bridge {
       versionFilePath = this.bridgePath.concat('\\versions.txt')
       versionFileExists = checkIfPathExists(versionFilePath)
     } else {
-      info('this.bridgePath.concat version'.concat(this.bridgePath.concat('/versions.txt')))
       versionFilePath = this.bridgePath.concat('/versions.txt')
       versionFileExists = checkIfPathExists(versionFilePath)
     }
@@ -397,16 +396,18 @@ export class Bridge {
   async validateBridgePath(): Promise<void> {
     this.bridgePath = this.getBridgeDefaultPath()
     if (BRIDGE_CLI_INSTALL_DIRECTORY_KEY) {
+      this.bridgePath = BRIDGE_CLI_INSTALL_DIRECTORY_KEY
+      if (!checkIfPathExists(this.bridgePath)) {
+        throw new Error(constants.BRIDGE_INSTALL_DIRECTORY_NOT_FOUND_ERROR)
+      }
       let folderName = 'bridge-cli-bundle-$version-$platform'.replace('$version', this.bridgeVersion).replace('$platform', this.getPlatform())
       if (process.platform === 'win32') {
         folderName = `\\${folderName}`
       } else if (process.platform === 'darwin' || process.platform === 'linux') {
         folderName = `/${folderName}`
       }
-      this.bridgePath = BRIDGE_CLI_INSTALL_DIRECTORY_KEY.concat(folderName)
-      info('check path directory '.concat(this.bridgePath))
-      if (!checkIfPathExists(this.bridgePath)) {
-        throw new Error(constants.BRIDGE_INSTALL_DIRECTORY_NOT_FOUND_ERROR)
+      if (checkIfPathExists(BRIDGE_CLI_INSTALL_DIRECTORY_KEY.concat(folderName))) {
+        this.bridgePath = BRIDGE_CLI_INSTALL_DIRECTORY_KEY.concat(folderName)
       }
     } else {
       if (ENABLE_NETWORK_AIR_GAP && !checkIfPathExists(this.getBridgeDefaultPath())) {
